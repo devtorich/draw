43e036ee3188b402de72b70242d6ba1774e30f35
daoyi7 <kawhichina@gmail.com>
2017-10-30 21:24:30 +0800

    clear

---
+++ js/draw.js
@@ -0,0 +1,60 @@
+
+  const log = console.log.bind(console)
+  const w = window.innerWidth
+  const h = window.innerHeight
+  const c = document.querySelector("#canvas")      // 获取canvas
+  const CclientRect = c.getBoundingClientRect()    // 获取canvas的各种坐标值
+  let ctx = c.getContext("2d")                    // 实例化一个2dcanvas
+  c.width = w-80                                   // 设置宽高
+  c.height = h
+
+  let d = false                                   // 初始化鼠标按下事件 初始时为false
+  let drawX                                       // 初始画鼠标按下的起始点
+  let drawY
+
+  c.addEventListener('touchstart', (e) => {
+    e.preventDefault()
+    d = true
+
+    drawX = e.targetTouches[0].pageX - CclientRect.x
+    drawY = e.targetTouches[0].pageY - CclientRect.y
+
+    ctx.beginPath()
+    ctx.moveTo(drawX, drawY)
+  })
+
+  c.addEventListener('touchend', (e) => {
+    d = false
+    ctx.closePath()
+  })
+
+  c.addEventListener('touchmove', (e) => {
+    if(d) {
+      alert(123)
+      ctx.lineTo(e.targetTouches[0].pageX - CclientRect.x, e.targetTouches[0].pageY - CclientRect.y)
+      ctx.stroke()
+    }
+  })
+
+  c.onmousedown = (e) => {
+    d = true
+
+    drawX = e.pageX - CclientRect.x
+    drawY = e.pageY - CclientRect.y
+
+    ctx.beginPath()
+    ctx.moveTo(drawX, drawY)
+  }
+
+  c.onmouseup = (e) => {
+    d = false
+    ctx.closePath()
+  }
+
+  c.onmousemove = (e) => {
+    if(d) {
+      log(drawX)
+      ctx.lineTo(e.pageX - CclientRect.x, e.pageY - CclientRect.y)
+      ctx.stroke()
+    }
+  }
