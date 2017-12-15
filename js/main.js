2f35baab649581d1211d027d968136bf71c4b04d
daoyi7 <kawhichina@gmail.com>
2017-12-15 11:17:18 +0800

    update move

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const log = console.log.bind(console)
+
+let d = false // 初始化鼠标按下事件
+let movePoint = []
+
 const main = document.querySelector('.main')
+const editArea = document.createElement('div')    // 小方块
+const editRotate = document.createElement('div')  // 小方块外拖拽按钮
+
+const mainLeft = main.offsetLeft
+const mainTop = main.offsetTop
 
 /**
  * 批量添加attribute
@@ -10,20 +19,19 @@ const setAttr = (el, attrs) => {
   }
 }
 
-const editArea = document.createElement('div')
-const editRotate = document.createElement('div')
-
+// 上、右、左、下、右上、左上、右下、左下
 const editDir = ['t', 'r', 'l', 'b', 'tr', 'tl', 'br', 'bl']
 
+// 添加 className
 setAttr(editArea, {
-  'class': 'edit-area'
+  'class': 'edit-area',
+  'style': 'width:150px;height:150px;transform:translate(437px, 293px);'
 })
 setAttr(editRotate, {
   'class': 'edit-rotate'
 })
 
 main.onmousedown = (e) => {
-  log(e.offsetX)
   main.append(editArea)
   editArea.appendChild(editRotate)
   for (var i = 0; i < editDir.length; i++) {
@@ -35,4 +43,21 @@ main.onmousedown = (e) => {
     })
     editArea.appendChild(editResize)
   }
+}
+
+editArea.onmousedown = (e) => {
+  d = true
+}
+
+editArea.onmousemove = (e) => {
+  if (d) {
+    let moveX = e.pageX - 75 - mainLeft
+    let moveY = e.pageY - 75 - mainTop
+
+    editArea.style.transform = 'translate('+ moveX +'px, '+ moveY +'px)'
+  }
+}
+
+editArea.onmouseup = (e) => {
+  d = false
 }
